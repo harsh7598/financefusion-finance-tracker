18bbf7481600ee7093785919124fb031db444c8d
harsh7598 <harryht7@gmail.com>
2024-09-17 12:11:06 +0530

    signup

--- src/components/SignupSignin/index.js
+++ src/components/SignupSignin/index.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./styles.css";
+import Input from '../input';
+import Button from '../Button';
 
 function SignUpSingInComponent() {
+const [name, setName] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [confirmPassword, setConfirmPassword] = useState("");
+
+function signupWithEmail(){
+  console.log(name,"name");
+  console.log(email,"email");
+}
   return (
     <div className='signup-wrapper'>
     <h2 className='title'>Sign Up on <span style={{color:"var(--theme)"}}>FinanceFusion</span>
     </h2>
+    <form>
+      <Input label={"Full Name"} state={name}
+      setState={setName}
+      placeholder={"John Doe"} /> 
+      <Input label={"Email"} state={email}
+      setState={setEmail}
+      placeholder={"johndoe@gmail.com"} /> 
+      <Input label={"Password"} state={password}
+      setState={setPassword}
+      placeholder={"example@123"} /> 
+      <Input label={"Confirm Password"} state={confirmPassword}
+      setState={setConfirmPassword}
+      placeholder={"example@123"} />
+      <Button text={ "Signup Using Email and Password" onClick={signupWithEmail} } />
+      <p style={{textAlign:"center"}}>or</p>
+      <Button text={ "Signup Using Google" } blue={true}/>
+    </form>
 </div>
   )
 }
--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import "./style.css";
 
-function Input() {
+function Input({label, state, setState, placeholder}) {
   return (
-    <div>Input</div>
+    <div className='input-wrapper'>
+      <p className='label-input'>{label}</p>
+      <input value={state} placeholder
+      = {placeholder}onChange={(e) => setState(e.target.value)}className='custom-input' />
+    </div>
   )
 }
 
