e1d08cbb723446b2f6119e2ac7c6018daf7fc4fe
harsh7598 <harryht7@gmail.com>
2024-09-19 14:33:07 +0530

    signupp

--- src/components/SignupSignin/index.js
+++ src/components/SignupSignin/index.js
@@ -1,41 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./styles.css";
-import Input from '../input';
-import Button from '../Button';
+import Input from "../input";
+import Button from "../Button";
 
 function SignUpSingInComponent() {
-const [name, setName] = useState("");
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
-const [confirmPassword, setConfirmPassword] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-function signupWithEmail(){
-  console.log(name,"name");
-  console.log(email,"email");
-}
+  function signupWithEmail() {
+    console.log("Name", name);
+    console.log("Email", email);
+    console.log("password", password);
+    console.log("confirmpassword", confirmPassword);
+    //Authentication of user using email and password
+    
+  }
   return (
-    <div className='signup-wrapper'>
-    <h2 className='title'>Sign Up on <span style={{color:"var(--theme)"}}>FinanceFusion</span>
-    </h2>
-    <form>
-      <Input label={"Full Name"} state={name}
-      setState={setName}
-      placeholder={"John Doe"} /> 
-      <Input label={"Email"} state={email}
-      setState={setEmail}
-      placeholder={"johndoe@gmail.com"} /> 
-      <Input label={"Password"} state={password}
-      setState={setPassword}
-      placeholder={"example@123"} /> 
-      <Input label={"Confirm Password"} state={confirmPassword}
-      setState={setConfirmPassword}
-      placeholder={"example@123"} />
-      <Button text={ "Signup Using Email and Password" onClick={signupWithEmail} } />
-      <p style={{textAlign:"center"}}>or</p>
-      <Button text={ "Signup Using Google" } blue={true}/>
-    </form>
-</div>
-  )
+    <div className="signup-wrapper">
+      <h2 className="title">
+        Sign Up on <span style={{ color: "var(--theme)" }}>FinanceFusion</span>
+      </h2>
+      <form>
+        <Input
+          label={"Full Name"}
+          state={name}
+          setState={setName}
+          placeholder={"John Doe"}
+        />
+        <Input
+          type="email"
+          label={"Email"}
+          state={email}
+          setState={setEmail}
+          placeholder={"johndoe@gmail.com"}
+        />
+        <Input
+          type="password"
+          label={"Password"}
+          state={password}
+          setState={setPassword}
+          placeholder={"example@123"}
+        />
+        <Input
+          type="password"
+          label={"Confirm Password"}
+          state={confirmPassword}
+          setState={setConfirmPassword}
+          placeholder={"example@123"}
+        />
+        <Button
+          text={"Signup Using Email and Password"}
+          onClick={signupWithEmail}
+        />
+        <p style={{ textAlign: "center" }}>or</p>
+        <Button text={"Signup Using Google"} blue={true} />
+      </form>
+    </div>
+  );
 }
 
-export default SignUpSingInComponent
+export default SignUpSingInComponent;
--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./style.css";
 
-function Input({label, state, setState, placeholder}) {
+function Input({label, state, setState, placeholder,type}) {
   return (
     <div className='input-wrapper'>
       <p className='label-input'>{label}</p>
-      <input value={state} placeholder
+      <input 
+    type={type}
+       value={state} placeholder
       = {placeholder}onChange={(e) => setState(e.target.value)}className='custom-input' />
     </div>
   )
